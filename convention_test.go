1cb7afcd48b2070c58c94c3cbdecb14a05096886
Vincent Thiery <vjmthiery@gmail.com>
2019-11-15 08:28:36 +0100

    Define the first conventions

---
+++ convention_test.go
@@ -0,0 +1,53 @@
+package daycount
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConventionString(t *testing.T) {
+	testCases := []struct {
+		name       string
+		convention Convention
+	}{
+		{
+			"ActualActual",
+			ActualActual,
+		},
+		{
+			"ActualActualAFB",
+			ActualActualAFB,
+		},
+		{
+			"ActualThreeSixty",
+			ActualThreeSixty,
+		},
+		{
+			"ActualThreeSixtyFiveFixed",
+			ActualThreeSixtyFiveFixed,
+		},
+		{
+			"ThirtyThreeSixtyUS",
+			ThirtyThreeSixtyUS,
+		},
+		{
+			"ThirtyThreeSixtyEuropean",
+			ThirtyThreeSixtyEuropean,
+		},
+		{
+			"ThirtyThreeSixtyItalian",
+			ThirtyThreeSixtyItalian,
+		},
+		{
+			"ThirtyThreeSixtyGerman",
+			ThirtyThreeSixtyGerman,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.name, tc.convention.String())
+		})
+	}
+}
